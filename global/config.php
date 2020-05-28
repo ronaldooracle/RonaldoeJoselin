7b0397c34c4f5dd67132ac5545f1d69959d778e0
Ronaldo Martins da Silva <30814275+ronaldooracle@users.noreply.github.com>
2020-05-28 13:36:42 -0400

    teste
    ts

---
+++ global/config.php
@@ -0,0 +1,9 @@
+<?php
+    define("KEY", "danico");
+    define("COD", "AES-128-ECB");
+
+    define("SERVIDOR", "localhost");
+    define("USUARIO", "root");
+    define("PASSWORD", "root123");
+    define("BD", "curso_tienda");
+?>
---
+++ templates/pie.php
@@ -0,0 +1,4 @@
+</div>
+<div class="text-center col-12">Danico 2019</div>
+</body>
+</html>
